ddb1f9890e7f57ef70c66990c60571d0bc15f680
Jijiluzana225 <jiji.luzana@gmail.com>
2026-06-27 12:42:39 +0800

    navigator.serviceWorker.radsfegister

--- habal_project/urls.py
+++ habal_project/urls.py
@@ -1,22 +1,13 @@
-from django.urls import path, include
 from django.contrib import admin
-from django.http import HttpResponse
+from django.urls import path, include
 from django.conf import settings
-from pathlib import Path
-
-
-def service_worker(request):
-    sw_path = settings.BASE_DIR / "habal_project" / "static" / "service-worker.js"
-
-    with open(sw_path, "r", encoding="utf-8") as file:
-        return HttpResponse(
-            file.read(),
-            content_type="application/javascript"
-        )
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("service-worker.js", service_worker, name="service_worker"),
-    path("", include("booking.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('booking.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
