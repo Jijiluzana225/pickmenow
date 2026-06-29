5919cf243d102a735c10cbcc2820f750596d879c
Jijiluzana225 <jiji.luzana@gmail.com>
2026-06-29 18:57:43 +0800

    adsfads

--- booking/sms.py
+++ booking/sms.py
@@ -30,3 +30,41 @@ def send_sms(phone, message):
             "error": "Not JSON response",
             "raw": response.text
         }
+    
+
+
+    import requests
+from requests.auth import HTTPBasicAuth
+
+UNISMS_SECRET_KEY = "sk_1d6c9ca4-841a-45d0-971d-83353750a916"
+SENDER_ID = "UnisoftDEV"   # Your approved Sender ID
+
+
+def send_sms(phone, message):
+    url = "https://unismsapi.com/api/sms"
+
+    payload = {
+        "recipient": phone,
+        "content": message,
+        "sender_id": SENDER_ID
+    }
+
+    response = requests.post(
+        url,
+        json=payload,
+        auth=HTTPBasicAuth(UNISMS_SECRET_KEY, ""),
+        headers={
+            "Content-Type": "application/json"
+        }
+    )
+
+    print("Status:", response.status_code)
+    print("Raw:", response.text)
+
+    try:
+        return response.json()
+    except Exception:
+        return {
+            "error": "Not JSON response",
+            "raw": response.text
+        }
