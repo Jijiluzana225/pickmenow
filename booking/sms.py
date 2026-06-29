0a803507dda0adea4b47e749584e3348d0232bcc
Jijiluzana225 <jiji.luzana@gmail.com>
2026-06-30 07:17:51 +0800

    adsfad

--- booking/sms.py
+++ booking/sms.py
@@ -1,39 +1,5 @@
+
 import requests
-from requests.auth import HTTPBasicAuth
-
-
-UNISMS_SECRET_KEY = "sk_1d6c9ca4-841a-45d0-971d-83353750a916"
-
-
-def send_sms(phone, message):
-    url = "https://unismsapi.com/api/sms"
-
-    response = requests.post(
-        url,
-        json={
-            "recipient": phone,
-            "content": message
-        },
-        auth=HTTPBasicAuth(UNISMS_SECRET_KEY, ""),
-        headers={
-            "Content-Type": "application/json"
-        }
-    )
-
-    print("Status:", response.status_code)
-    print("Raw:", response.text)
-
-    try:
-        return response.json()
-    except:
-        return {
-            "error": "Not JSON response",
-            "raw": response.text
-        }
-    
-
-
-    import requests
 from requests.auth import HTTPBasicAuth
 
 UNISMS_SECRET_KEY = "sk_1d6c9ca4-841a-45d0-971d-83353750a916"
