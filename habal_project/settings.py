e50110acc607c71b480eff1c68639ebfb86bdb12
Jijiluzana225 <jiji.luzana@gmail.com>
2026-07-11 14:42:49 +0800

    debug off

--- habal_project/settings.py
+++ habal_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-exbdib5w!ml+4u4ovt(6u!xl8$h0r0-^pi1h762&-r1g04y9+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
